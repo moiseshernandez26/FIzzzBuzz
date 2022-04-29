186a36dbd8754a17c8fe516feaf0bab89f2e8be4
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-04-29 12:17:11 -0500

    chore: run linter

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,22 +1,22 @@
-describe('Suit test for ExplorerService', () => {
-  const Reader = require('../../lib/utils/Reader')
-  const ExplorerService = require('../../lib/services/ExplorerService')
-  test('1) FilterByMission', () => {
-    const explorers = Reader.readJsonFile('explorers.json')
-    const res = ExplorerService.filterByMission(explorers, 'node')
-    expect(res.length).toBe(10)
-  })
-  test('2) GetAmountOfExplorersByMission', () => {
-    const explorers = Reader.readJsonFile('explorers.json')
-    const res = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
-    expect(res).toBe(10)
-  })
-  test('3) GetExplorersUsernamesByMission', () => {
-    const explorers = Reader.readJsonFile('explorers.json')
-    const res = ExplorerService.getExplorersUsernamesByMission(
-      explorers,
-      'node'
-    )
-    expect(res.length).toBe(10)
-  })
-})
+describe("Suit test for ExplorerService", () => {
+    const Reader = require("../../lib/utils/Reader");
+    const ExplorerService = require("../../lib/services/ExplorerService");
+    test("1) FilterByMission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const res = ExplorerService.filterByMission(explorers, "node");
+        expect(res.length).toBe(10);
+    });
+    test("2) GetAmountOfExplorersByMission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const res = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(res).toBe(10);
+    });
+    test("3) GetExplorersUsernamesByMission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const res = ExplorerService.getExplorersUsernamesByMission(
+            explorers,
+            "node"
+        );
+        expect(res.length).toBe(10);
+    });
+});
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,34 +1,34 @@
 describe("Suit test for ExplorerService", () => {
-  const FizzbuzzService = require("../../lib/services/FizzbuzzService");
-  test("1) Explorer con score 1", () => {
-    const explorer = { name: "Explorer 1", score: 1 };
-    const res = FizzbuzzService.applyValidationInExplorer(explorer);
-    expect(res.trick).toBe(1);
-  });
+    const FizzbuzzService = require("../../lib/services/FizzbuzzService");
+    test("1) Explorer con score 1", () => {
+        const explorer = { name: "Explorer 1", score: 1 };
+        const res = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(res.trick).toBe(1);
+    });
 
-  test("2) Explorer con score 3", () => {
-    const explorer = { name: "Explorer 1", score: 3 };
-    const res = FizzbuzzService.applyValidationInExplorer(explorer);
-    expect(res.trick).toBe("FIZZ");
-  });
-  test("3) Explorer con score 5", () => {
-    const explorer = { name: "Explorer 1", score: 5 };
-    const res = FizzbuzzService.applyValidationInExplorer(explorer);
-    expect(res.trick).toBe("BUZZ");
-  });
-  test("4) Explorer con score 15", () => {
-    const explorer = { name: "Explorer 1", score: 15 };
-    const res = FizzbuzzService.applyValidationInExplorer(explorer);
-    expect(res.trick).toBe("FIZZBUZZ");
-  });
-  test("5) Valor de score no es un numero", () => {
-    const explorer = { name: "Explorer 1", score: "a" };
-    const res = FizzbuzzService.applyValidationInExplorer(explorer);
-    expect(res.trick).not.toBe("a");
-  });
-  test("6) Validar con un explorer vacio", () => {
-    const explorer = {};
-    const res = FizzbuzzService.applyValidationInExplorer(explorer);
-    expect(res.trick).toBe(undefined);
-  });
+    test("2) Explorer con score 3", () => {
+        const explorer = { name: "Explorer 1", score: 3 };
+        const res = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(res.trick).toBe("FIZZ");
+    });
+    test("3) Explorer con score 5", () => {
+        const explorer = { name: "Explorer 1", score: 5 };
+        const res = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(res.trick).toBe("BUZZ");
+    });
+    test("4) Explorer con score 15", () => {
+        const explorer = { name: "Explorer 1", score: 15 };
+        const res = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(res.trick).toBe("FIZZBUZZ");
+    });
+    test("5) Valor de score no es un numero", () => {
+        const explorer = { name: "Explorer 1", score: "a" };
+        const res = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(res.trick).not.toBe("a");
+    });
+    test("6) Validar con un explorer vacio", () => {
+        const explorer = {};
+        const res = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(res.trick).toBe(undefined);
+    });
 });
